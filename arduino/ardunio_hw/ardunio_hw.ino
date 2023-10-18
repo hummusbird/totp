8de78fd80f6dc90eec8e5db08778546967e3c05a
hummusbird <birb@birb.cc>
2023-10-18 09:40:57 +0100

    reorg + quicker bar updates

--- arduino/ardunio_hw/ardunio_hw.ino
+++ arduino/ardunio_hw/ardunio_hw.ino
@@ -9,7 +9,6 @@ static DS1307 RTC;
 // OLED PREP
 // some OLEDs do not like working with other devices on the i2c bus, so we have to connect the display to pins A2 and A3 and run software i2c instead.
 U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/A3, /* data=*/A2, /* reset=*/U8X8_PIN_NONE);
-int updateDisplay;
 
 void u8g2_prepare() {
   u8g2.setFont(u8g2_font_profont11_tf);
@@ -18,22 +17,30 @@ void u8g2_prepare() {
   u8g2.setFontPosTop();
 }
 
-// TOTP PREP
-int totp_count = 3;
-char totp_names[3][10] = { "12345",
-                           "09876",
-                           "ABCDEF" };
+void drawCountdownBars(long TIME) {
+  if (30 - (int)(TIME % 30) > 22) { u8g2.drawBox(110, 17, 18, 3); };
+  if (30 - (int)(TIME % 30) > 15) { u8g2.drawBox(110, 23, 18, 3); };
+  if (30 - (int)(TIME % 30) > 7) { u8g2.drawBox(110, 29, 18, 3); };
+}
 
-TOTP totps[3] = { TOTP("12345678901234567890", 20, 30),
-                  TOTP("09876543210987654321", 20, 30),
-                  TOTP("ABCDEFGHIJABCDEFGHIJ", 20, 30) };
+// TOTP PREP
+static const int totp_count = 3;
+char totp_names[totp_count][10] = { "12345",
+                                    "09876",
+                                    "ABCDEF" };
+
+TOTP totps[totp_count] = { TOTP("12345678901234567890", 20, 30),
+                           TOTP("09876543210987654321", 20, 30),
+                           TOTP("ABCDEFGHIJABCDEFGHIJ", 20, 30) };
 char code[7];
 
 // BUTTON PREP
 const int buttonPin = 7;
 int pageNum = 0;
-int buttonState;     // current button state
-int lastState;       // last button state
+int buttonState;  // current button state
+int lastState;    // last button state
+
+long lastMillis;
 
 void setup() {
   RTC.begin();
@@ -44,6 +51,8 @@ void setup() {
 }
 
 void loop() {
+  int updateDisplay = 0;
+  int updateBar = 0;
   int buttonState = digitalRead(buttonPin);
 
   if (buttonState == HIGH && lastState != buttonState) {  // update display if the button has been pressed
@@ -63,9 +72,8 @@ void loop() {
   }
 
   int barInt = 30 - (int)(TIME % 30);
-
-  if (barInt == 22 || barInt == 15 || barInt == 7) {
-    updateDisplay = 1;
+  if (barInt == 22 || barInt == 15 || barInt == 7) {  // update display if new countdown bar should be shown
+    updateBar = 1;
   }
 
   // Serial.print("UNIX: ");
@@ -73,27 +81,35 @@ void loop() {
   // Serial.print("CODE: ");
   // Serial.println(newCode);
   // Serial.println("=======================");
-
+  
   // u8g2.drawStr(115, 10, String(30 - (int)(TIME % 30)).c_str());
 
   if (updateDisplay == 1) {
     u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_profont11_tf);
+    u8g2.setFontMode(1);
+    u8g2.setFont(u8g2_font_spleen16x32_mn);  // this font is only digits, to save on memory
+    u8g2.drawStr(0, 5, newCode);             // draw large TOTP code
 
+    u8g2.setFont(u8g2_font_profont11_tf);
     char pages[6];
     snprintf(pages, sizeof(pages), "%d%s%d", pageNum + 1, "/", totp_count);
-    u8g2.drawStr(110, 0, pages);
+    u8g2.drawStr(110, 0, pages);              // draw page counter
+    u8g2.drawStr(0, 0, totp_names[pageNum]);  // draw TOTP name
 
-    u8g2.drawStr(0, 0, totp_names[pageNum]);
-    u8g2.setFont(u8g2_font_fur20_tn);
-    u8g2.drawStr(0, 10, newCode);
-
-    if (30 - (int)(TIME % 30) > 22) { u8g2.drawBox(110, 15, 16, 3); };
-    if (30 - (int)(TIME % 30) > 15) { u8g2.drawBox(110, 21, 16, 3); };
-    if (30 - (int)(TIME % 30) > 7) { u8g2.drawBox(110, 27, 16, 3); };
+    drawCountdownBars(TIME);
 
     u8g2.sendBuffer();
   }
 
+  if (updateBar == 1) {
+    // draw countdown boxes
+    u8g2.setDrawColor(0);
+    u8g2.drawBox(110, 17, 18, 15);
+    u8g2.setDrawColor(1);
+    drawCountdownBars(TIME);
+    u8g2.updateDisplayArea(13, 2, 3, 3);
+  }
+
   updateDisplay = 0;
+  updateBar = 0;
 }
